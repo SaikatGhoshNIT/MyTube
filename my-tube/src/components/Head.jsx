40d9b57c5c73e8ba917fa74cf4b04eb4f301567b
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-29 02:08:49 +0530

    creating header like youtube

--- my-tube/src/components/Head.jsx
+++ my-tube/src/components/Head.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import YoutubeSearchedForTwoToneIcon from '@mui/icons-material/YoutubeSearchedForTwoTone';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 
 function Head() {
   return (
-    <div>
+    <div className="grid grid-flow-col">
       <div>
-        <img src="" alt="" />
+        <MenuRoundedIcon fontSize="large"/>
+        <img src="https://t3.ftcdn.net/jpg/05/07/46/84/240_F_507468479_HfrpT7CIoYTBZSGRQi7RcWgo98wo3vb7.jpg" alt="Logo" />
+      </div>
+      <div>
+        <input type="text" />
+        <button><YoutubeSearchedForTwoToneIcon fontSize="small"/></button>
+        <button><KeyboardVoiceIcon fontSize="large"/></button>
+      </div>
+      <div>
+        <button><NotificationsActiveIcon fontSize="large"/></button>
+        <button><AccountCircleIcon fontSize="large"/></button>
       </div>
     </div>
   )
