837bc9db74218a5f967a3f40b31a9f1cfee0b5b2
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-28 21:55:56 +0530

    removed MaterialUI Icons

--- my-tube/src/components/Head.jsx
+++ my-tube/src/components/Head.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+
 
 function Head() {
   return (
     <div>
       <div>
-        <MenuRoundedIcon/>
+        <img src="" alt="" />
       </div>
     </div>
   )
