7c6b7708ffc78b2636aaf1e2d297e607a2f79641
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-03-31 02:17:43 +0530

    Add Css to make the Header like youtube

--- my-tube/src/components/Head.jsx
+++ my-tube/src/components/Head.jsx
@@ -8,19 +8,20 @@ import logo from "../undraw_video_files_fu10.svg";
 
 function Head() {
   return (
-    <div className="grid grid-flow-col">
-      <div className="flex">
-        <button><MenuRoundedIcon fontSize="large"/></button>
-        <img src={logo} alt="Logo" />
+    <div className="flex flex-row items-center justify-between py-0 px-4 h-[54px] shadow-lg">
+      <div className="flex items-center justify-center">
+        <button className='pr-3'><MenuRoundedIcon sx={{ fontSize: 30 }}/></button>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhL0D8KyXbvENy862ZywENdz7idTIu5h-ASA&usqp=CAU" alt="Logo" className="h-12"/>
+        <span className="font-bold font-serif text-lg text-red-500">MyTube</span>
       </div>
-      <div>
+      <div className=''>
         <input type="text" className="border-solid"/>
         <button><YoutubeSearchedForTwoToneIcon fontSize="small"/></button>
-        <button><KeyboardVoiceIcon fontSize="large"/></button>
+        <button className='p-[1px] rounded-full bg-red-200 ml-8'><KeyboardVoiceIcon sx={{ fontSize: 30 }}/></button>
       </div>
-      <div>
-        <button><NotificationsActiveIcon fontSize="large"/></button>
-        <button><AccountCircleIcon fontSize="large"/></button>
+      <div className='flex justify-between pr-5'>
+        <button className='pl-4'><NotificationsActiveIcon  sx={{ fontSize: 30 }}/></button>
+        <button className='pl-4'><AccountCircleIcon  sx={{ fontSize: 30 }}/></button>
       </div>
     </div>
   )
