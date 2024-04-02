aff963833b606808168cd2c02ff7b687d4164b92
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-04-02 23:49:00 +0530

    rollback

--- my-tube/src/App.js
+++ my-tube/src/App.js
@@ -7,8 +7,7 @@ import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <Provider store={store}>
-      
+    <Provider store={store}> 
     <div className="App">
       <Head/>
       <Body/>
