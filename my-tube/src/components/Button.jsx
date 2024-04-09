beba6640c69da9b65fff5e08cf5b62e10a8630b0
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-04-10 01:36:19 +0530

    button

--- my-tube/src/components/Button.jsx
+++ my-tube/src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({name}) {
   return (
     <div>
-        <button className='px-3 py-1 my-5 mx-[6px] bg-slate-400 font-semibold hover:font-bold rounded-lg hover:bg-black hover:text-white'>{name}</button>
+        <button className='px-3 py-1 my-5 mx-[6px] bg-slate-400 font-semibold hover:font-bold rounded-lg active:bg-black active:text-white'>{name}</button>
     </div>
   )
 }
--- my-tube/src/components/ButtonList.jsx
+++ my-tube/src/components/ButtonList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import Button from './Button'
 
 function ButtonList() {
-  const list = ["All", "Live", "Cricket", "Best For You", "Cooking", "Virat", "Messi", "News", "Election"]
+  const list = ["All", "Live", "Cricket", "For You", "Cooking", "Virat", "Messi", "News", "Election", "Football", "JEE", "Bengali", "Biriyani", "Century", "Roast", "Sunday Suspense", "Durga Puja"]
   return (
-    <div className="flex">
+    <div className="flex overflow-x-auto whitespace-nowrap ">
       {list.map(listItem => (<Button name={listItem}/>))}
     </div>
   )
