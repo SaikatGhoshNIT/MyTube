d2563ecac4c34f27fcb61a35af36bf08f907ee4c
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-04-11 18:48:16 +0530

    API and API key

---
+++ my-tube/src/utils/constant.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY = "AIzaSyA4eyOj_uD46w5jP6b2JtCjZc3MiQnPpdk"
+
+const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/videos?snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY
+
+export {YOUTUBE_API};
