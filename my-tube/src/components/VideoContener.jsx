16fb55849de0411ef5386aaba67b936311c5d1ed
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-04-12 01:08:24 +0530

    ,

--- my-tube/src/components/VideoContener.jsx
+++ my-tube/src/components/VideoContener.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useEffect } from 'react'
+import {YOUTUBE_API} from '../utils/constant.js'
 
 function VideoContener() {
+
+useEffect(()=>{
+  getVideos()
+},[])
+
+const getVideos = async () => {
+  const data = await fetch(YOUTUBE_API)
+  const dataJSON = await data.json()
+  console.log(dataJSON);
+}
+
   return (
     <div>VideoContener</div>
   )
--- my-tube/src/utils/constant.js
+++ my-tube/src/utils/constant.js
@@ -1,5 +1,5 @@
-const GOOGLE_API_KEY = "AIzaSyA4eyOj_uD46w5jP6b2JtCjZc3MiQnPpdk"
+const GOOGLE_API_KEY = "AIzaSyD1EeHCYHf5lX4wzzfBtobrncYOEYsTZfk"
 
-const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/videos?snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY
+//const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/videos?snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+GOOGLE_API_KEY
 
-export {YOUTUBE_API};
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=60&regionCode=IN&key=" + GOOGLE_API_KEY ;
