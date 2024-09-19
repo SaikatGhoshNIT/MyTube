76afb8d1e73839c434b4c9827427bae28ee0fc5d
SaikatGhoshNIT <saikatghosh5434@gmail.com>
2024-09-20 01:34:32 +0530

    VideCard issue resolved with maxVideos count of 50

--- my-tube/src/components/VideoCard.jsx
+++ my-tube/src/components/VideoCard.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 
-function VideoCard(props) {
+function VideoCard({info}) {
 
-    const {info} = props;
+    //const {info} = props;
 
-    console.log(info);
+    //console.log(info);
 
-    //const {snippet, statistics} = info;
+    const {snippet, statistics} = info;
 
-    //const {channelTitle, title, thumbnails} = snippet
+    const {channelTitle, title, thumbnails} = snippet
     
 
   return (
-    <div className='p-2 m-2 w-80 shadow-lg rounded-lg'>
-        {/*<img className='rounded-lg' src={thumbnails.medium.url} alt="Thumbnail" />
-        <ul>
-            <li>{title}</li>
+    <div className='p-2 m-2 w-80 shadow-lg'>
+        <img className='rounded-lg' src={thumbnails.medium.url} alt="Thumbnail" />
+        <ul className='p-2'>
+            <li className='font-bold pb-2'>{title}</li>
             <li>{channelTitle}</li>
             <li>Views: {statistics.viewCount}</li>
-        </ul>*/}
+        </ul>
+        
     </div>
   )
 }
--- my-tube/src/components/VideoContener.jsx
+++ my-tube/src/components/VideoContener.jsx
@@ -5,23 +5,36 @@ import VideoCard from './VideoCard.jsx';
 
 function VideoContener() {
 
-const [videos, setVideos] = useState();
+  const [videos, setVideos] = useState([]);
 
-useEffect(()=>{
-  getVideos()
-},[])
+  useEffect(() => {
+    getVideos();
+  }, []);
 
-const getVideos = async () => {
-  const data = await fetch(YOUTUBE_API)
-  const dataJSON = await data.json()
-  console.log(dataJSON);
-  //setVideos(dataJSON.items)
-}
+  const getVideos = async () => {
+    const data = await fetch(YOUTUBE_API);
+    const dataJSON = await data.json();
+    
+    console.log(dataJSON.items[0]);  // This will log the first item from the API
+    setVideos(dataJSON.items);       // This sets the state asynchronously
+  };
+
+ // Log videos after the state has updated
+//  useEffect(() => {
+//   console.log(videos);  // This will log when `videos` is updated
+// }, [videos]);  // Runs this effect whenever `videos` changes
 
   return (
     <div className='p-5 m-2 flex flex-wrap'>
-      {/*videos.map(video =><VideoCard key={video.id} info={video}/>)*/}  
-      <VideoCard info={videos[0]}/>
+      {/*videos.map(video =><VideoCard key={video.id} info={video}/>)*/}
+      {/*<VideoCard info={videos[0]}/>*/} 
+      {videos.length > 0 ? (
+        videos.map((video) => (
+          <VideoCard key={video.id} info={video} />
+        ))
+      ) : (
+        <p>Loading videos...</p>  // Fallback when videos are still being fetched
+      )}
     </div>
   )
 }
